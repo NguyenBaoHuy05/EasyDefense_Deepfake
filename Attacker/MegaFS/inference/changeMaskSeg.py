551a8259259f5125672293ddf3bb8ecf80343989
NguyenBaoHuy05 <baohuy30122005@gmail.com>
2025-06-18 11:38:15 +0700

    MegaFS

---
+++ Attacker/MegaFS/inference/changeMaskSeg.py
@@ -0,0 +1,39 @@
+import os
+import cv2
+import numpy as np
+
+label = {
+    "background": 0,
+    "skin": 1,
+    "l_brow": 2,
+    "r_brow": 3,
+    "l_eye": 4,
+    "r_eye": 5,
+    "eye_g": 6,
+    "l_ear": 7,
+    "r_ear": 8,
+    "ear_r": 9,
+    "nose": 10,
+    "mouth": 11,
+    "u_lip": 12,
+    "l_lip": 13,
+    "neck": 14,
+    "neck_l": 15,
+    "cloth": 16,
+    "hair": 17,
+    "hat": 18,
+}
+for file in os.listdir("mask"):
+    if file.endswith(".png"):
+        file_path = os.path.join("mask", file)
+        face = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+        segmentation = np.zeros_like(face, dtype=np.uint8)
+    break
+for file in os.listdir("mask"):
+    if file.endswith(".png"):
+        file_path = os.path.join("mask", file)
+        imgs = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+        idx, att = file_path.split("_")
+        att = att.split(".")[0]
+        segmentation[imgs == 255] = label[att]
+cv2.imwrite(f"maskSeg/{int(idx)}.png", segmentation)
